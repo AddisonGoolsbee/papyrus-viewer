ea271465acb2e34e2a7f02593f503d90ba82e820
Alexa Schor <alexaschor@gmail.com>
2023-04-25 12:22:44 -0400

    Only rate-limit wholeflip button when animation is enabled

--- index.js
+++ index.js
@@ -294,10 +294,14 @@ $(document).ready(function () {
 });
 
 function flipWhole() {
-  $("#flipWholeButton").prop("disabled", true);
-  setTimeout(function () {
-    $("#flipWholeButton").prop("disabled", false);
-  }, 600);
+
+  if ($("#enableAnimate").is(":checked")) {
+    $("#flipWholeButton").prop("disabled", true);
+    setTimeout(function () {
+      $("#flipWholeButton").prop("disabled", false);
+    }, 600);
+  }
+
   const imageContainer = document.getElementById("imageContainer");
   const images = imageContainer.querySelectorAll("img");
   const div = imageContainer;

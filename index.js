5b50ed04eb1eaf8fa3b531ad158338371877263b
Alexa Schor <alexaschor@gmail.com>
2023-04-25 10:58:09 -0400

    Add jQuery draggable patch to eliminate jumping when moving a rotated fragment

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 // python3 -m http.server 8000
+
+// jQuery draggable patch to support CSS transforms
+//   Workaround for https://bugs.jqueryui.com/ticket/6844
+//   From https://stackoverflow.com/a/18606402
+(function($) {
+  var __dx, __dy;
+  var __recoupLeft, __recoupTop;
+
+  var parseIntSafe = function(value) {
+    return (function(n) {
+      return isNaN(n) ? 0 : n;
+    })(parseInt(value, 10));
+  }
+
+  $.fn.draggablePatched = function(options) {
+    options = options || {};
+    return this.draggable({
+                          cursor: options.cursor || 'move',
+                          zIndex: 100,
+                          drag: function(event, ui) {
+                            __dx = ui.position.left - ui.originalPosition.left;
+                            __dy = ui.position.top - ui.originalPosition.top;
+                            ui.position.left = ui.originalPosition.left + __dx + __recoupLeft;
+                            ui.position.top = ui.originalPosition.top + __dy + __recoupTop;
+                            if (options.drag) {
+                              options.drag(event, ui);
+                            }
+                          },
+                          start: function(event, ui) {
+                            var left = parseIntSafe($(this).css('left'));
+                            var top = parseIntSafe($(this).css('top'));
+                            __recoupLeft = left - ui.position.left;
+                            __recoupTop = top - ui.position.top;
+                            if (options.start) {
+                              options.start(event, ui);
+                            }
+                          },
+                          stop: function(event, ui) {
+                            if (options.stop) {
+                              options.stop(event, ui);
+                            }
+                          },
+                          create: function(event, ui) {
+                            if (options.create) {
+                              options.create(event, ui);
+                            }
+                          }
+    });
+  }
+})(jQuery);
+
+
 function fragmentFlip() {
 
   if ($(this).attr("src").includes("background.png")) {
@@ -57,7 +109,7 @@ function loadFragmentImages(fragments, path) {
   $("#imageContainer img").dblclick(fragmentFlip);
   //console.log('yes container')
 
-  $("#imageContainer img").draggable({
+  $("#imageContainer img").draggablePatched({
                                      containment: 'img[src$="background.png"]',
 
                                      start: function () {

76feae83aa2b7e993dc55ab4b842d598225ea499
Alexa Schor <alexaschor@gmail.com>
2023-04-26 16:04:04 -0400

    Add some of the image enhancement methods from [Atanasiu & Marthot‐Santaniello 2021]: vividness enhancement, retinex, hue shift

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ function fragmentFlip() {
   var filename = filename_full.split('_')[0]
   var side = filename_full.split('_')[1]
   paragraph.innerHTML = "text of " + side + " side of " + filename + ' is: Unknown';
-  //console.log('new')
 
   if ($("#enableAnimate").is(":checked")) {
     let duration = 300;
@@ -382,14 +381,308 @@ function removeRotateHandle() {
   $(".rotate-handle").remove();
 }
 
+function lab2rgb(lab){
+  var y = (lab[0] + 16) / 116,
+    x = lab[1] / 500 + y,
+    z = y - lab[2] / 200,
+    r, g, b;
+
+  x = 0.95047 * ((x * x * x > 0.008856) ? x * x * x : (x - 16/116) / 7.787);
+  y = 1.00000 * ((y * y * y > 0.008856) ? y * y * y : (y - 16/116) / 7.787);
+  z = 1.08883 * ((z * z * z > 0.008856) ? z * z * z : (z - 16/116) / 7.787);
+
+  r = x *  3.2406 + y * -1.5372 + z * -0.4986;
+  g = x * -0.9689 + y *  1.8758 + z *  0.0415;
+  b = x *  0.0557 + y * -0.2040 + z *  1.0570;
+
+  r = (r > 0.0031308) ? (1.055 * Math.pow(r, 1/2.4) - 0.055) : 12.92 * r;
+  g = (g > 0.0031308) ? (1.055 * Math.pow(g, 1/2.4) - 0.055) : 12.92 * g;
+  b = (b > 0.0031308) ? (1.055 * Math.pow(b, 1/2.4) - 0.055) : 12.92 * b;
+
+  return [Math.max(0, Math.min(1, r)) * 255,
+    Math.max(0, Math.min(1, g)) * 255,
+    Math.max(0, Math.min(1, b)) * 255]
+}
+
+
+function rgb2lab(rgb){
+  var r = rgb[0] / 255,
+    g = rgb[1] / 255,
+    b = rgb[2] / 255,
+    x, y, z;
+
+  r = (r > 0.04045) ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+  g = (g > 0.04045) ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+  b = (b > 0.04045) ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+  x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+  y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+  z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+  x = (x > 0.008856) ? Math.pow(x, 1/3) : (7.787 * x) + 16/116;
+  y = (y > 0.008856) ? Math.pow(y, 1/3) : (7.787 * y) + 16/116;
+  z = (z > 0.008856) ? Math.pow(z, 1/3) : (7.787 * z) + 16/116;
+
+  return [(116 * y) - 16, 500 * (x - y), 200 * (y - z)]
+}
+
+
+function enhanceVividness(imageData, negative) {
+  const width = imageData.width;
+  const height = imageData.height;
+  const data = imageData.data;
+
+  let minVividness = 99999;
+  let maxVividness = -99999;
+
+  // First we find the bounds of the vividness
+  for (let i = 0; i < data.length; i += 4) {
+    // Extract RGB values
+    const r = data[i];
+    const g = data[i + 1];
+    const b = data[i + 2];
+
+    // Convert RGB to LAB
+    let [L, A, B] = rgb2lab([r, g, b]);
+
+    // Apply transformation to L component
+    if (negative) {
+      L = 100 - Math.sqrt(L * L + A * A + B * B);
+    } else {
+      L = Math.sqrt(L * L + A * A + B * B);
+    }
+
+    if (L > maxVividness) {
+      maxVividness = L;
+    }
+
+    if (L < minVividness) {
+      minVividness = L;
+    }
+
+  }
+
+  // Loop over every pixel
+  for (let i = 0; i < data.length; i += 4) {
+    // Extract RGB values
+    const r = data[i];
+    const g = data[i + 1];
+    const b = data[i + 2];
+
+    // Convert RGB to LAB
+    let [L, A, B] = rgb2lab([r, g, b]);
+
+    // Apply transformation to L component
+    if (negative) {
+      L = 100 - Math.sqrt(L * L + A * A + B * B);
+    } else {
+      L = Math.sqrt(L * L + A * A + B * B);
+    }
+
+    // Scale to bounds
+    L = ( (L - minVividness)/(maxVividness - minVividness) ) * 100;
+
+    // Convert LAB back to RGB
+    const [rOut, gOut, bOut] = lab2rgb([L, A, B]);
+
+    // Set modified RGB values back onto the ImageData object
+    data[i] = rOut;
+    data[i + 1] = gOut;
+    data[i + 2] = bOut;
+  }
+
+  // Create a new ImageData object from the modified pixel data and return it
+  return new ImageData(data, width, height);
+}
+
+function blueShift(imageData) {
+  const width = imageData.width;
+  const height = imageData.height;
+  const data = imageData.data;
+
+  // Loop over every pixel
+  for (let i = 0; i < data.length; i += 4) {
+    // Extract RGB values
+    const r = data[i];
+    const g = data[i + 1];
+    const b = data[i + 2];
+
+    // Convert RGB to LAB
+    let [L, A, B] = rgb2lab([r, g, b]);
+
+    A *= -1;
+    B *= -1;
+
+    // Convert LAB back to RGB
+    const [rOut, gOut, bOut] = lab2rgb([L, A, B]);
+
+    // Set modified RGB values back onto the ImageData object
+    data[i] = rOut;
+    data[i + 1] = gOut;
+    data[i + 2] = bOut;
+  }
+
+  // Create a new ImageData object from the modified pixel data and return it
+  return new ImageData(data, width, height);
+}
+
+function invert(imageData) {
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i];         // invert red
+    data[i + 1] = 255 - data[i + 1]; // invert green
+    data[i + 2] = 255 - data[i + 2]; // invert blue
+  }
+
+  return imageData;
+}
+
+function retinex(imageData, sigma, gain, offset) {
+  const data = imageData.data;
+  const width = imageData.width;
+  const height = imageData.height;
+  const size = width * height;
+
+  const R = new Float32Array(size);
+  const G = new Float32Array(size);
+  const B = new Float32Array(size);
+  const L = new Float32Array(size);
+
+  // Convert the input RGB values to float values
+  for (let i = 0; i < size; i++) {
+    R[i] = data[i * 4] / 255;
+    G[i] = data[i * 4 + 1] / 255;
+    B[i] = data[i * 4 + 2] / 255;
+  }
+
+  // Calculate the logarithm of the input RGB values
+  for (let i = 0; i < size; i++) {
+    L[i] = Math.log10(R[i] + G[i] + B[i]);
+  }
+
+  // Apply the Retinex algorithm
+  const blurred = new Float32Array(size);
+  const output = new Float32Array(size);
+  const kernelSize = Math.ceil(sigma * 3) * 2 + 1;
+  const kernel = new Float32Array(kernelSize);
+
+  for (let i = 0; i < kernelSize; i++) {
+    const x = i - Math.floor(kernelSize / 2);
+    kernel[i] = Math.exp(-(x * x) / (2 * sigma * sigma));
+  }
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let sum = 0;
+      let weight = 0;
+
+      for (let j = 0; j < kernelSize; j++) {
+        const x2 = x + j - Math.floor(kernelSize / 2);
+        if (x2 < 0 || x2 >= width) {
+          continue;
+        }
+
+        const index = y * width + x2;
+        // const distance = j - Math.floor(kernelSize / 2);
+        const w = kernel[j];
+
+        sum += w * L[index];
+        weight += w;
+      }
+
+      blurred[y * width + x] = sum / weight;
+    }
+  }
+
+  for (let i = 0; i < size; i++) {
+    output[i] = gain * (L[i] - blurred[i]) + offset;
+  }
+
+  // Convert the output values back to RGB values
+  for (let i = 0; i < size; i++) {
+    const value = Math.pow(10, output[i]);
+    data[i * 4] = Math.min(Math.max(Math.round(value * R[i] * 255), 0), 255);
+    data[i * 4 + 1] = Math.min(Math.max(Math.round(value * G[i] * 255), 0), 255);
+    data[i * 4 + 2] = Math.min(Math.max(Math.round(value * B[i] * 255), 0), 255);
+  }
+
+  return new ImageData(data, width, height);
+}
+
+
+function closeOverlay() {
+  $(".enhanceOverlay").remove();
+  $(".enhanceCloseButton").remove();
+}
+
+function enhance() {
+  console.log("Enhancing...");
+  $("#enhanceSelector").parent().append(`<div class="loader"></div>`)
+  $("#enhanceButton").toggle();
+  $("#imageContainer").css("height", $("#background").height());
+  html2canvas($("#imageContainer").get(0)).then( function(orig) {
+    let ctx = orig.getContext("2d");
+    const origData = ctx.getImageData(0, 0, orig.width, orig.height);
+
+    let newData, choice = $("#enhanceSelector").children("option:selected").val();
+
+    if (choice === "negative") {
+      newData = invert(origData);
+    } else if (choice == "vividness") {
+      newData = enhanceVividness(origData, false);
+    } else if (choice == "neg_vividness") {
+      newData = enhanceVividness(origData, true);
+    } else if (choice == "blue") {
+      newData = blueShift(origData);
+    } else if (choice == "vivid_blue") {
+      newData = enhanceVividness(origData, false);
+      newData = blueShift(newData);
+    } else if (choice == "neg_vivid_blue") {
+      newData = enhanceVividness(origData, true);
+      newData = blueShift(newData);
+    } else if (choice == "retinex") {
+      newData = retinex(origData, 15, 2, 0);
+    } else if (choice == "neg_retinex") {
+      newData = retinex(origData, 15, 2, 0);
+      newData = enhanceVividness(newData, true);
+    } else if (choice == "retinex_blue") {
+      newData = retinex(origData, 15, 2, 0);
+      newData = blueShift(newData);
+    } else if (choice == "neg_retinex_blue") {
+      newData = retinex(origData, 15, 2, 0);
+      newData = enhanceVividness(newData, true);
+      newData = blueShift(newData);
+    }
+
+    ctx.putImageData(newData, 0, 0);
+
+    // let t = $("#background").offset().top;
+    // let l = $("#background").offset().left;
+    let w = $("#background").width();
+    let h = $("#background").height();
+
+    let overlay = $(`<div class="enhanceOverlay" style="width:${w}; height:${h}; position:absolute; z-index:900;"><img src="${orig.toDataURL()}" style="width:100%;height:100%"/></div>`);
+    $("#imageContainer").append(overlay);
+    $("#imageContainer").append(`<div class="enhanceCloseButton" style="position:absolute; top:0px; right:0px; z-index:901" onclick="closeOverlay()">⨂</div>`);
+
+
+    $("#enhanceButton").toggle();
+    $(".loader").remove();
+  });
+}
+
 $(document).ready(function() {
   $('#toggleButton').click(function() {
     if ($('body').css('cursor') === 'zoom-in') {
       $('body').css('cursor', '');
       $(this).removeClass('active');
+      $(".enhanceOverlay").trigger("zoom.destroy");
     } else {
       $('body').css('cursor', 'zoom-in');
       $(this).addClass('active');
+      $(".enhanceOverlay").zoom({ magnify: 3 });
     }
   });
+  $("#enhanceButton").on("click", enhance);
 });
